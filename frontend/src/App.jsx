adf79a26d24fbbb2da0a41690ca2df5bf27b7fcb
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2026-02-17 23:35:34 +0530

    installed react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-linear-to-l from-primary via-pink-400 to-secondary p-20 rounded-2xl border border-dashed border-white">
+          <p className="capitalize font-bold font-mono text-white text-8xl">comming soon....</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
