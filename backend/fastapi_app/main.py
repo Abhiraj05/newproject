0e6111f76cf90da91bd50524b46213b6dc928cfa
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2026-04-27 23:26:10 +0530

    installed chat gemini and embeddings model

--- backend/fastapi_app/main.py
+++ backend/fastapi_app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
 
@@ -13,7 +17,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
 
 @app.get("/")
-async def main():
-    return {"message": "Hello World"}
+async def scan_doc():
+    model = ChatGoogleGenerativeAI(
+        model="gemini-3-flash-preview",
+        temperature=1.0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2
+    )
+
+    messages = [
+        (
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ),
+        ("human", "I love programming."),
+    ]
+    ai_msg = model.invoke(messages)
+    print(ai_msg.content)
+
+
+@app.get("/generate_embeddings")
+async def generate_embeddings():
+    embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
+    vector = embeddings.embed_query("hello, world!")
+    print(vector[:5])
