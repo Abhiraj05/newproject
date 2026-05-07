893c23b91a665c3ef1d13c5e89aff70b824fc484
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2026-05-07 22:41:00 +0530

    created forgot password and reset password pages & wrt

--- backend/django_app/project/user/urls.py
+++ backend/django_app/project/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from user.views import UserRegistration,UserLogin
+from user.views import UserRegistration,UserLogin,ForgotPassword,SetNewPassword
 
 urlpatterns = [
     path('register/',UserRegistration.as_view(),name="user_registration"),
-    path('login/',UserLogin.as_view(),name="user_login")
+    path('login/',UserLogin.as_view(),name="user_login"),
+    path('forgot_password/',ForgotPassword.as_view(),name="forgot_password"),
+    path('reset_password/',SetNewPassword.as_view(),name="reset_password")
 ]
--- backend/django_app/project/user/views.py
+++ backend/django_app/project/user/views.py
@@ -7,6 +7,10 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from user.serializers import UserRegisterFormSerializer
+from common.email import send_email
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 # Create your views here.
 
@@ -67,3 +71,82 @@ class UserLogin(APIView):
 
             except:
                 return Response({"message": "form submission failed !"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# generates the reset link email & sends it to user
+class ForgotPassword(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(request):
+        reset_email = request.data.get("email")
+        user = User.objects.filter(email=reset_email).first()
+        if not user:
+            return Response({"message": "user not found!"}, status=status.HTTP_404_NOT_FOUND)
+
+        uid = urlsafe_base64_encode(force_bytes(user.id))
+        token = PasswordResetTokenGenerator().make_token(user)
+        default_email = "support@legalnext.com"
+        mail_sub = "Password Reset Link"
+
+        message = f"""
+        Hello,
+
+        You requested a password reset for your account. Click on the link below to set a new password:
+
+        http://localhost:5173/setnewpassword/?uid={uid}&token={token}
+
+        If you did not request this, please ignore this email.
+
+        Best regards,
+        legalnext Support Team
+        """
+
+        try:
+            send_email(request, default_email, reset_email, message, mail_sub)
+        except Exception as e:
+            print(f"Reset email failed: {str(e)}")
+
+        return Response({"message": "Reset link generated and sent to your email."})
+
+
+# sets the new password in database
+class SetNewPassword(APIView):
+    def post(request):
+        uid = request.data.get('uid')
+        token = request.data.get('token')
+        new_password = request.data.get('new_password')
+
+        if not uid or not token or not new_password:
+            return Response({"message": "uid, token and new_password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user_id = urlsafe_base64_decode(uid).decode()
+            user = User.objects.get(id=user_id)
+        except:
+            return Response({"message": "invalid uid"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if PasswordResetTokenGenerator().check_token(user, token):
+            user.set_password(new_password)
+            user.save()
+
+            default_email = "support@legalnext.com"
+            mail_sub = "Your Password Has Been Changed"
+            confirm_message = f"""
+            Hello {user.username},
+
+            This is a confirmation that the password for your account has been successfully changed.
+
+            If you did not perform this action, please contact our support team immediately.
+
+            Best regards,
+            legalnext Support Team
+            """
+            try:
+                send_email(request, default_email, user.email,
+                        confirm_message, mail_sub)
+            except Exception as e:
+                print(f"Confirmation email failed: {str(e)}")
+
+            return Response({"message": "password reset successfully."})
+        else:
+            return Response({"message": "Invalid or expired token.."}, status=status.HTTP_400_BAD_REQUEST)
