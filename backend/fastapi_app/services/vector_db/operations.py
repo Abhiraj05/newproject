44c72a33f61dc6e58d5b47d1581b34b45a3ca5f2
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2026-05-07 20:37:19 +0530

    created RAG pipeline

--- backend/fastapi_app/services/vector_db/operations.py
+++ backend/fastapi_app/services/vector_db/operations.py
@@ -1,20 +1,21 @@
 from .collections import get_collections
+import uuid
 
 collection = get_collections()
 
-def add_embedding(doc_id, text, embedding, metadata):
 
+def add_embedding(chunks, embeddings):
     collection.add(
-        ids=[doc_id],
-        documents=[text],
-        embeddings=[embedding],
-        metadatas=[metadata]
+        ids=[str(uuid.uuid4()) for _ in chunks],
+        documents=chunks,
+        embeddings=embeddings
     )
+
+
 
 def query_embedding(user_id, query_embedding, top_k=5):
     return collection.query(
         query_embeddings=[query_embedding],
         n_results=top_k,
-        where = {"user_id":user_id}
+        where={"user_id": user_id}
     )
-    
